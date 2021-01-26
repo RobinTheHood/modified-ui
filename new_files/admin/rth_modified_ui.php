e356d0819480886b842d81963a7808a19232fae0
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-01-26 17:15:51 +0100

    feat: improve examples

--- new_files/admin/rth_modified_ui.php
+++ new_files/admin/rth_modified_ui.php
@@ -8,11 +8,12 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL ^ E_WARNING ^ E_NOTICE);
 
-\RobinTheHood\ModifiedUi\Classes\Examples\PageExample1::run();
+\RobinTheHood\ModifiedUi\Classes\Examples\Example1::run();
+// \RobinTheHood\ModifiedUi\Classes\Examples\PageExample1::run();
 // \RobinTheHood\ModifiedUi\Classes\Examples\PanelExample1::run();
 // \RobinTheHood\ModifiedUi\Classes\Examples\TableExample1::run();
 // \RobinTheHood\ModifiedUi\Classes\Examples\FormExample1::run();
 // \RobinTheHood\ModifiedUi\Classes\Examples\FormExample2::run();
 // \RobinTheHood\ModifiedUi\Classes\Examples\FormExample3::run();
 // \RobinTheHood\ModifiedUi\Classes\Examples\TabPanelExample1::run();
-// RobinTheHood\ModifiedUi\Classes\Examples\Example1::run();
+// \RobinTheHood\ModifiedUi\Classes\Examples\JsEventExample1::run();
